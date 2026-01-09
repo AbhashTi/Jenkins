e12471279c214832cc4e6d8bf9784d5ef5fcca55
AbhashTi <106645093+AbhashTi@users.noreply.github.com>
2026-01-09 18:12:40 +0530

    multiplication in java

---
+++ mul.java
@@ -0,0 +1,10 @@
+public class mul{
+public static void main(String[] args){
+
+int x=Integer.parseInt(args[0]);
+int y=Integer.parseInt(args[1]);
+for(int i=1;i<=y;i++){
+System.out.println(x+" X "+i+" = "+i*x);
+}
+}
+}
