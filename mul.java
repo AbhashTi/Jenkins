680b6eaeafe09b8dfe270d845b32768b5eea25ba
AbhashTi <106645093+AbhashTi@users.noreply.github.com>
2026-02-20 11:19:54 +0530

    Correct multiplication logic and output in mul.java
    Fix multiplication output format and calculation.

--- mul.java
+++ mul.java
@@ -4,7 +4,7 @@ public static void main(String[] args){
 int x=Integer.parseInt(args[0]);
 int y=Integer.parseInt(args[1]);
 for(int i=1;i<=y;i++){
-System.out.println(x+" X "+i+" = "+i*x);
+System.out.println(x+" X "+i=x*i);
 }
 }
 }
